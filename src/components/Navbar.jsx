ec56f3a39d12e928911040490d099dc58f666ef9
Ekemini <ekemini456@gmail.com>
2026-09-01 15:07:02 +0100

    Edited the website

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,60 @@
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink, useLocation } from 'react-router-dom'
 
 function Navbar() {
+  const location = useLocation()
+
+  const isHomeActive = location.pathname === '/' && !location.hash
+  const isPortfolioActive = location.pathname === '/' && location.hash === '#portfolio'
+
   return (
     <nav className="navbar" aria-label="Main navigation">
       <Link className="navbar__brand" to="/">E.k Pic</Link>
       <ul className="navbar__links">
-        <li><NavLink className={({ isActive }) => isActive ? 'is-active' : ''} to="/" end>Home</NavLink></li>
-        <li><NavLink className={({ isActive }) => isActive ? 'is-active' : ''} to="/#portfolio">Portfolio</NavLink></li>
-        <li><NavLink className={({ isActive }) => isActive ? 'is-active' : ''} to="/about">About</NavLink></li>
-        <li><NavLink className={({ isActive }) => isActive ? 'is-active' : ''} to="/services">Services</NavLink></li>
-        <li><NavLink className={({ isActive }) => isActive ? 'is-active' : ''} to="/contact">Contact</NavLink></li>
+        <li>
+          <NavLink
+            className={() => isHomeActive ? 'is-active' : ''}
+            to="/"
+            end
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={() => isPortfolioActive ? 'is-active' : ''}
+            to="/#portfolio"
+          >
+            Portfolio
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive && !location.hash) ? 'is-active' : ''}
+            to="/about"
+          >
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive && !location.hash) ? 'is-active' : ''}
+            to="/services"
+          >
+            Services
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive && !location.hash) ? 'is-active' : ''}
+            to="/contact"
+          >
+            Contact
+          </NavLink>
+        </li>
       </ul>
     </nav>
   )
 }
 
 export default Navbar
+
